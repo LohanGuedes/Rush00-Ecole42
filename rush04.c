d2ec3e3896fa44ba7d63cab8ffe60d0a15e46a62
igor <igorrturano@gmail.com>
2022-02-04 22:54:37 -0300

    opa

--- rush04.c
+++ rush04.c
@@ -6,35 +6,34 @@
 /*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/04 23:43:25 by coder             #+#    #+#             */
-/*   Updated: 2022/02/04 23:43:25 by coder            ###   ########.fr       */
+/*   Updated: 2022/02/05 02:53:47 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-// y > z > x // x > y
+
 void	ft_putchar(char c);
+
 void	rush(int x, int y)
 {
-	int lines;
-	int cols;
+	int	lines;
+	int	cols;
 
-    if (x < 1 || y < 1)
-    {
-        return;
-    }
-
+	if (x < 1 || y < 1)
+		return ;
 	lines = 1;
-	while(lines <= y)
+	while (lines <= y)
 	{
 		cols = 1;
-		while(cols <= x)
+		while (cols <= x)
 		{
-			if((lines == 1 && cols == 1) || (lines == y && cols == x && lines != 1 && cols != 1))
+			if ((lines == 1 && cols == 1)
+				|| (lines == y && cols == x && lines != 1 && cols != 1))
 				ft_putchar('A');
-			else if((lines == 1 && cols == x) || (lines == y && cols == 1))
+			else if ((lines == 1 && cols == x) || (lines == y && cols == 1))
 				ft_putchar('C');
-			else if((lines == 1  && cols != 1 && cols != x)
-            || (cols == 1 && lines != 1 && lines != y)
-            || (cols == x && lines != 1 && lines != y)
-            || (lines == y && cols != 1 && cols != x))
+			else if ((lines == 1 && cols != 1 && cols != x)
+				|| (cols == 1 && lines != 1 && lines != y)
+				|| (cols == x && lines != 1 && lines != y)
+				|| (lines == y && cols != 1 && cols != x))
 				ft_putchar('B');
 			else
 				ft_putchar(' ');
